5e6425aaf155991347d220357d7b1b5dadb7601a
gianmarco-todesco <gianmarco.todesco@gmail.com>
2020-02-14 00:17:09 +0100

    .

--- pages/bubbles/bubbles.js
+++ pages/bubbles/bubbles.js
@@ -48,6 +48,15 @@ function populateScene() {
 let stop = false
 
 function tick() {
+    const theta = performance.now() * 0.0001;
+    const cs = Math.cos(theta);
+    const sn = Math.sin(theta);
+    model.mesh.material.setMatrix('rot4', BABYLON.Matrix.FromArray([
+        cs,0,0,-sn,
+        0,1,0,0,
+        0,0,1,0,
+        sn,0,0,cs
+    ]))
 }
 
 
@@ -61,11 +70,11 @@ uffa = {}
 class PolychoronBubbleModel {
     constructor() {
         this.mesh = this.buildMesh(5)
-        this.mesh.rotation.x=Math.PI/2
+       //  this.mesh.rotation.x=Math.PI/2
     }
 
     buildMesh(m) {
-        let n = 5
+        let n = 20
         let mesh = this.mesh = new BABYLON.Mesh("custom", scene);
         let positions = []
         let indices = []
@@ -127,7 +136,49 @@ class PolychoronBubbleModel {
         uffa.vertexData = vertexData
         vertexData.applyToMesh(mesh)
 
+        let data = PolychoronData.p120
+        m = data.faces.length
+        for(let i=1; i<m; i++) {
+            mesh.createInstance("uff-"+i)
+        }
+
         mesh.material = this.createShaderMaterial()
+
+        function assign(array, i, p) { 
+            array[4*i]=p.x
+            array[4*i+1]=p.y 
+            array[4*i+2]=p.z
+            array[4*i+3]=p.w
+        }
+        const originArray = new Float32Array(m*4)
+        const e0Array = new Float32Array(m*4)
+        const e1Array = new Float32Array(m*4)
+        data.faces.forEach((f,i) => {
+            let pts = f.map(j=>data.vertices[j])
+            let center = new BABYLON.Vector4(0,0,0,0);
+            pts.forEach(p=>{center.addInPlace(p)})
+            center.scaleInPlace(1/f.length)
+
+            let e0 = pts[0].subtract(center)
+            let r = e0.length()
+            e0.scaleInPlace(1/r)
+            let e1 = pts[1].subtract(center)
+            let d = e1.x*e0.x+e1.y*e0.y+e1.z*e0.z+e1.w*e0.w
+            e1 = e1.subtract(e0.scale(d)).normalize()
+
+            assign(originArray,i,center)
+            assign(e0Array,i,e0.scale(r))
+            assign(e1Array,i,e1.scale(r))            
+        })
+        
+        let buffer = new BABYLON.Buffer(engine, originArray , true, 4, false, true);
+        mesh.setVerticesBuffer(buffer.createVertexBuffer("origin", 0, 4))
+        buffer = new BABYLON.Buffer(engine, e0Array , true, 4, false, true);
+        mesh.setVerticesBuffer(buffer.createVertexBuffer("e0", 0, 4))
+        buffer = new BABYLON.Buffer(engine, e1Array , true, 4, false, true);
+        mesh.setVerticesBuffer(buffer.createVertexBuffer("e1", 0, 4))
+
+
         return mesh
 
 
@@ -141,18 +192,20 @@ class PolychoronBubbleModel {
             },
             {
                 attributes: [
-                    "position", "uv", "color", "phi_cssn",
-                    "origin", "e0", "e1", 
+                    "position", "origin","e0","e1" 
                 ],
                 uniforms: [
                     "world", "worldView", 
                     "worldViewProjection", 
-                    "view", "projection", 
-                    "time",
-                    
-                    "u_cs", "u_sn"]
+                    "view", "projection",
+                    "rot4"
+                ]
             });
         mat.backFaceCulling = false
+        mat.alpha = 0.2
+        
+        mat.setMatrix('rot4', BABYLON.Matrix.Identity())
+
         return mat
     }
 }
@@ -169,18 +222,14 @@ class PolychoronBubbleModel {
 
     // Attributes
     attribute vec3 position;
+    attribute vec4 origin, e0, e1;
 
     // Uniforms
-    uniform mat4 worldViewProjection, world;
-    uniform float time;
-    uniform vec4 origin, e0, e1; 
+    uniform mat4 worldViewProjection, world, worldView;
+    uniform mat4 rot4;
 
-    // Normal
-    varying vec2 vUV;
     varying vec3 v_norm;
     varying vec3 v_pos;
-    varying float err;
-    varying vec3 v_color;
     varying vec3 v_surfaceToLight;
     varying vec3 v_surfaceToView;
 
@@ -189,46 +238,41 @@ class PolychoronBubbleModel {
 
     vec3 fun(float u, float v) {   
         
-        vec4 pos = origin + u * e0 + v * e1;
+        vec4 pos =rot4 * (origin + u * e0 + v * e1);
         pos = normalize(pos);
-        float k = 2.0 / (1.0 - pos.w);
+        float k = 0.5 / (1.0 - pos.w);
         return vec3(pos.x * k, pos.y * k, pos.z * k);
     }
 
     void main(void) {
+        vec2 uv = vec2(position.x, position.z);
 
-        vec3 p = fun(position.x, position.z);
-        float epsilon = 0.00001;
-        vec3 dpdu = fun(position.x+epsilon, position.z) - p;
-        vec3 dpdv = fun(position.x, position.z+epsilon) - p;
+        vec3 p = fun(uv.x, uv.y);
+        float epsilon = 0.001;
+        vec3 dpdu = fun(uv.x+epsilon, uv.y) - fun(uv.x-epsilon, uv.y);
+        vec3 dpdv = fun(uv.x, uv.y+epsilon) - fun(uv.x, uv.y-epsilon) ;
         vec3 norm = normalize(cross(dpdu, dpdv));
-        /*
-        v_color = vec3(0.8,0.3,0.1);
-        v_pos = p;
-        gl_Position = worldViewProjection * vec4(p, 1.0);
-        */
+        
 
         gl_Position = worldViewProjection * vec4(p, 1.0);
-        v_norm = vec3(world * vec4(norm, 0.0));
+        v_norm = (worldView * vec4(norm, 0.0)).xyz;
 
         /*
         vUV = vec2(position.x, position.z);
         //if(p.x*p.x+p.y*p.y+p.z*p.z>100.0) err = 1.0;
         //else err = 0.0;
+        */
 
         v_surfaceToLight = vec3(0.0,10.0,0.0) - (world * vec4(p,1.0)).xyz;
         v_surfaceToView = (vec4(0.0,0.0,10.0,1.0) - (world * vec4(p,1.0))).xyz; // u_viewInverse[3] 
-      */
     }
 `
-    // ---------------------------------------------------------
-    BABYLON.Effect.ShadersStore[shaderName + "FragmentShader"]= `
+
+BABYLON.Effect.ShadersStore[shaderName + "FragmentShader"]= `
     precision highp float;
-    varying vec2 vUV;
+
     varying vec3 v_norm;
-    varying vec3 v_pos;
-    varying float err;
-    varying vec3 v_color;
+    varying vec3 v_pos;    
     varying vec3 v_surfaceToLight;
     varying vec3 v_surfaceToView;
 
@@ -239,44 +283,19 @@ class PolychoronBubbleModel {
                     1.0);
     }
 
-
-    // uniform sampler2D textureSampler;
-    void main(void) {
-        //if(err > 0.0 || abs(v_pos.x) > 2.0 || abs(v_pos.y) > 2.0 || abs(v_pos.z) > 2.0) discard;
-        //else 
-        {
-            vec3 norm = normalize(v_norm);
-            vec3 surfaceToLight = normalize(v_surfaceToLight);
-            vec3 surfaceToView = normalize(v_surfaceToView);
-            vec3 halfVector = normalize(surfaceToLight + surfaceToView);
-          
-            if(dot(surfaceToView, norm)<0.0) {  norm = -norm; }
-            float cs = dot(norm, surfaceToLight);
-            vec4 litR = lit(cs,dot(norm, halfVector), 120.0);
-  
-            vec3 color = v_color * litR.y + vec3(1.0,1.0,1.0) * litR.z;
-            gl_FragColor = vec4(color,1.0); // texture2D(textureSampler, vUV);    
-        }
-    }
-` 
-BABYLON.Effect.ShadersStore[shaderName + "FragmentShader"]= `
-    precision highp float;
-    varying vec2 vUV;
-    varying vec3 v_norm;
-    varying vec3 v_pos;
-    varying float err;
-    varying vec3 v_color;
-    varying vec3 v_surfaceToLight;
-    varying vec3 v_surfaceToView;
-
-    
-
     void main(void) {
         vec3 norm = normalize(v_norm);
-        float cs = norm.z;
-        if(cs<0.0) cs = -cs;
-        vec3 color = cs * vec3(0.3,0.4,0.6);
-        gl_FragColor = vec4(color,1.0) ;         
+        vec3 surfaceToLight = normalize(v_surfaceToLight);
+        vec3 surfaceToView = normalize(v_surfaceToView);
+        vec3 halfVector = normalize(surfaceToLight + surfaceToView);
+        
+        if(dot(surfaceToView, norm)<0.0) {  norm = -norm; }
+        float cs = dot(norm, surfaceToLight);
+        vec4 litR = lit(cs,dot(norm, halfVector), 120.0);
+
+        vec3 v_color = vec3(0.2,0.2,0.2);
+        vec3 color = v_color * litR.y + vec3(1.0,1.0,1.0) * litR.z;
+        gl_FragColor = vec4(color,0.2); // texture2D(textureSampler, vUV);      
     }
 ` 
 // ---------------------------------------------------------
