a31b0235310bb54a9a853ec84131348dc57964c3
gianmarco-todesco <gianmarco.todesco@gmail.com>
2020-02-20 02:07:28 +0100

    .

--- pages/yendred/yendred.js
+++ pages/yendred/yendred.js
@@ -50,8 +50,8 @@ function populateScene() {
         width: 10,
         height: 10,
     }, scene)
-    paper.position.z = 1
-    paper.isVisible = false
+    paper.position.z = -6
+    // paper.isVisible = false
     let mat = paper.material = new BABYLON.StandardMaterial('paper-mat', scene)
     mat.backFaceCulling = false
     mat.diffuseTexture = new BABYLON.Texture('images/yendred3.png')
@@ -83,8 +83,8 @@ class Polyhedron extends GeometricModel {
     constructor(data, scene) {
         super('polyhedron', scene)
         this.beginUpdate()
-        data.vertices.forEach(p=> this.addVertex(p, 0.1))
-        data.edges.forEach(([a,b])=> this.addEdge(data.vertices[a], data.vertices[b],0.05))
+        data.vertices.forEach(p=> this.addVertex(p, 0.05))
+        data.edges.forEach(([a,b])=> this.addEdge(data.vertices[a], data.vertices[b],0.02))
         let facePts = data.faces[0].map(i=>data.vertices[i])
         this.addFace(facePts)
         this.endUpdate()
@@ -99,10 +99,11 @@ class MyShadowGenerator {
             height: 10,
         }, scene)
         paper.position.y = 0.01
-    
+        // paper.position.x = 4
+
         let mat = paper.material = new BABYLON.StandardMaterial('paper-mat', scene)
         mat.backFaceCulling = false
-        mat.diffuseColor.set(0.3,0.3,0.3)
+        mat.diffuseColor.set(0.6,0.6,0.6)
         mat.specularColor.set(0,0,0)    
         paper.material.diffuseTexture = this.rtt
 
@@ -125,9 +126,16 @@ class MyShadowGenerator {
         let rttMaterial = new BABYLON.StandardMaterial('rtt-mat', scene)
         rttMaterial.diffuseColor.set(0,0,0)
         rttMaterial.specularColor.set(0,0,0)
-        rttMaterial.emissiveColor.set(0.1,0.7,0.85)
+        rttMaterial.emissiveColor.set(0.7,0.7,0.7)
         rttMaterial.ambientColor.set(0,0,0)
         rttMaterial.backFaceCulling = false
+
+        let rttMaterial2 = new BABYLON.StandardMaterial('rtt-mat2', scene)
+        rttMaterial2.diffuseColor.set(0,0,0)
+        rttMaterial2.specularColor.set(0,0,0)
+        rttMaterial2.emissiveColor.set(0.7,0.7,0.3)
+        rttMaterial2.ambientColor.set(0,0,0)
+        rttMaterial2.backFaceCulling = false
 
         let bgColor = new BABYLON.Color3()
     
@@ -135,17 +143,17 @@ class MyShadowGenerator {
     
         rtt.onBeforeRender = (e) => {
             rtt.renderList.forEach(mesh => {
-                if(mesh.getClassName() == "Mesh" && mesh.name != 'polyhedron-faces') {
+                if(mesh.getClassName() == "Mesh") {
                     mesh._saved = mesh.material
-                    mesh.material =  rttMaterial    
+                    mesh.material = mesh.name == 'polyhedron-faces' ? rttMaterial2 : rttMaterial    
                 }
             })
             bgColor.copyFrom(scene.clearColor)
-            scene.clearColor.set(.9,.9,.9)
+            scene.clearColor.set(1,1,1)
         }
         rtt.onAfterRender = () => {
             rtt.renderList.forEach(mesh => {
-                if(mesh.getClassName() == "Mesh" && mesh.name != 'polyhedron-faces') {
+                if(mesh.getClassName() == "Mesh" ) {
                     mesh.material =  mesh._saved
                 }
             })
